5bcf3f28d0ed1a52aef30279479e4201891ec629
jasmeen14 <jasmeen6199@gmail.com>
2022-02-18 10:20:10 +0100

    created website

---
+++ main.js
@@ -0,0 +1,13 @@
+(function () {
+    ('#WAButton').floatingWhatsApp({
+        phone: '(+91)8595419756', //WhatsApp Business phone number
+        headerTitle: 'Typically replies in minutes', //Popup Title
+        popupMessage: 'Hello, how can I help you?', //Popup Message
+        showPopup: true, //Enables popup display
+        buttonImage: '<img src ="whatsapp.jpg"/>', //Button Image
+        //headerColor: 'crimson', //Custom header color
+        //backgroundColor: 'crimson', //Custom background button color
+        position: "right" //Position: left | right
+
+    });
+});
